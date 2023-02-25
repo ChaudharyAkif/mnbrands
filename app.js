075733a1106e94c2f0c09f22be3e72e21bdaae27
M AKIF <makifhussainmn@gmail.com>
2023-02-25 20:30:33 +0500

    first commit practice javascript how are you

---
+++ app.js
@@ -0,0 +1,35 @@
+document.getElementById("akif").onclick = function(){
+
+let message = "Good day ";
+let name = "akif";
+let banger ="!";
+
+console.log ( message + name + banger);
+document.getElementById("aa").innerHTML = " akif"
+
+}
+
+
+
+let a = prompt+1;
+let b = 10;
+
+
+document.getElementById("akif").onclick = function (){
+
+
+  alert ( "akif")
+console.log ("akif")
+document.write("ahmed")
+};
+
+
+let arry = [ "ahmed" , "chaudhary","mnbrands","abdullahchaudhary"];
+
+console.log (arry[1])
+
+
+
+let arry2 = newarry [ "akahmed" , "akchaudhary","akmnbrands","akabdullahchaudhary"];
+
+console.log (arry)
